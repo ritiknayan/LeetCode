54bdb6576b9b574959138f96dbea1e82eb5f39ba
ritiknayan <115357292+ritiknayan@users.noreply.github.com>
2024-07-20 18:42:14 +0530

    Time: 0 ms (100%), Space: 8.2 MB (31.3%) - LeetHub

--- 1078-remove-outermost-parentheses/1078-remove-outermost-parentheses.cpp
+++ 1078-remove-outermost-parentheses/1078-remove-outermost-parentheses.cpp
@@ -1,29 +1,63 @@
+// class Solution {
+// public:
+//     string removeOuterParentheses(string s) {
+
+//     //     string ans="";
+         
+//     //   bool flag = true;
+//     //   int count=0;
+//     //   for(int i =0;i<s.length();i++){
+//     //     if(s[i] == '(') count++;
+//     //     else if(s[i] == ')') count--;
+
+
+//     //     if(count == 1 && flag == true){
+//     //         flag = false;
+//     //         continue;
+//     //     }
+//     //     if(count == 0 && flag == false){
+//     //         flag = true;
+//     //         continue;
+//     //     }
+//     //  ans=ans+s[i];
+
+//     //   }
+
+//     //   return ans;
+
+    
+
+//     }
+// };
+
 class Solution {
 public:
+    // Function to remove outermost parentheses of every primitive string in the
+    // decomposition of s
     string removeOuterParentheses(string s) {
+        string result;   // To store the final result
+        int balance = 0; // To keep track of the balance of parentheses
 
-        string ans="";
-         
-      bool flag = true;
-      int count=0;
-      for(int i =0;i<s.length();i++){
-        if(s[i] == '(') count++;
-        else if(s[i] == ')') count--;
-
-
-        if(count == 1 && flag == true){
-            flag = false;
-            continue;
+        // Iterate through each character in the string
+        for (int i = 0; i < s.size(); i++) {
+            if (s[i] == '(') {
+                // If balance is greater than 0, it means this '(' is not an
+                // outermost parenthesis
+                if (balance > 0) {
+                    result += s[i]; // Add the character to the result
+                }
+                balance++; // Increase the balance for '('
+            } else {
+                balance--; // Decrease the balance for ')'
+                // If balance is greater than 0, it means this ')' is not an
+                // outermost parenthesis
+                if (balance > 0) {
+                    result += s[i]; // Add the character to the result
+                }
+            }
         }
-        if(count == 0 && flag == false){
-            flag = true;
-            continue;
-        }
-     ans=ans+s[i];
 
-      }
-
-      return ans;
-
+        return result; // Return the final result after removing outermost
+                       // parentheses
     }
 };
