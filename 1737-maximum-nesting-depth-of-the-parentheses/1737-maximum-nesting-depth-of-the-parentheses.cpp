7402d06be594d91a385ec4848083ed53ff5c6701
ritiknayan <115357292+ritiknayan@users.noreply.github.com>
2024-07-22 12:30:55 +0530

    Time: 3 ms (41.83%), Space: 7.6 MB (33.84%) - LeetHub

--- 1737-maximum-nesting-depth-of-the-parentheses/1737-maximum-nesting-depth-of-the-parentheses.cpp
+++ 1737-maximum-nesting-depth-of-the-parentheses/1737-maximum-nesting-depth-of-the-parentheses.cpp
@@ -1,17 +1,36 @@
+// class Solution {
+// public:
+//     int maxDepth(string s) {
+//         int ans = 0;
+
+//         stack<char> st;
+//         for (char c : s) {
+//             if (c == '(') {
+//                 st.push(c);
+//             } else if (c == ')') {
+//                 st.pop();
+//             }
+            
+//             ans = max(ans, (int)st.size());
+//         }
+        
+//         return ans;
+//     }
+// };
 class Solution {
 public:
     int maxDepth(string s) {
         int ans = 0;
+        int openBrackets = 0;
 
-        stack<char> st;
         for (char c : s) {
             if (c == '(') {
-                st.push(c);
+                openBrackets++;
             } else if (c == ')') {
-                st.pop();
+                openBrackets--;
             }
             
-            ans = max(ans, (int)st.size());
+            ans = max(ans, openBrackets);
         }
         
         return ans;
