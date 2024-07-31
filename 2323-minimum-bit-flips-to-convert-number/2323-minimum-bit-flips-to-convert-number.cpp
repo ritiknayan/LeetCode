f8937aa189a412e1000035cf6dd431f614d241df
ritiknayan <115357292+ritiknayan@users.noreply.github.com>
2024-07-31 18:34:33 +0530

    Time: 0 ms (100%), Space: 7.7 MB (12.54%) - LeetHub

--- 2323-minimum-bit-flips-to-convert-number/2323-minimum-bit-flips-to-convert-number.cpp
+++ 2323-minimum-bit-flips-to-convert-number/2323-minimum-bit-flips-to-convert-number.cpp
@@ -2,15 +2,20 @@ class Solution {
 public:
     int minBitFlips(int start, int goal) {
         // XOR to find differing bits
-        int differingBits = start ^ goal;
+        // int differingBits = start ^ goal;
         
-        // Count the number of set bits (1s) in differingBits
-        int count = 0;
-        while (differingBits > 0) {
-            count += differingBits & 1; // Add the last bit of differingBits to count
-            differingBits >>= 1; // Shift right by 1 to process the next bit
-        }
+        // // Count the number of set bits (1s) in differingBits
+        // int count = 0;
+        // while (differingBits > 0) {
+        //     count += differingBits & 1; // Add the last bit of differingBits to count
+        //     differingBits >>= 1; // Shift right by 1 to process the next bit
+        // }
         
-        return count;
+        // return count;
+        int diffbits = start ^ goal;
+
+        int n = __builtin_popcount(diffbits);
+
+        return n;
     }
 };
