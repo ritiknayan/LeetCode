1b2e4585b741024a6f5cb3be6f80eff080938589
ritiknayan <115357292+ritiknayan@users.noreply.github.com>
2024-08-03 16:01:05 +0530

    Time: 4 ms (55.97%), Space: 14.9 MB (20.34%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.cpp
+++ 0031-next-permutation/0031-next-permutation.cpp
@@ -1,10 +1,32 @@
 class Solution {
 public:
     void nextPermutation(vector<int>& nums) {
+        int n = nums.size() - 1;
+        int index = -1;
 
-        next_permutation(nums.begin(),nums.end());
+        // Find the first index from the end where nums[i] < nums[i + 1]
+        for (int i = n - 1; i >= 0; i--) {
+            if (nums[i] < nums[i + 1]) {
+                index = i;
+                break;
+            }
+        }
 
-      
-        
+        if (index == -1) {
+            // If no such index is found, reverse the entire array
+            reverse(nums.begin(), nums.end());
+            return;
+        }
+
+        // Find the smallest number greater than nums[index] to the right of index
+        for (int i = n; i > index; i--) {
+            if (nums[i] > nums[index]) {
+                swap(nums[i], nums[index]);
+                break;
+            }
+        }
+
+        // Reverse the part of the array to the right of index
+        reverse(nums.begin() + index + 1, nums.end());
     }
 };
