87c79547b4a482106c422c8e5ad5999734f46c2a
ritiknayan <115357292+ritiknayan@users.noreply.github.com>
2024-08-24 22:42:46 +0530

    Time: 27 ms (83.94%), Space: 24.8 MB (74.2%) - LeetHub

---
+++ 1408-find-the-smallest-divisor-given-a-threshold/1408-find-the-smallest-divisor-given-a-threshold.cpp
@@ -0,0 +1,25 @@
+class Solution {
+private:
+    bool check(vector<int>& nums, int divisor, int threshold) {
+        int sum = 0;
+        for(auto&num : nums){
+            sum +=(num + divisor - 1) / divisor; //to divide num by divisor and round up the result.
+            if (sum > threshold) return false; // Early exit if sum exceeds threshold
+        }
+        return true;
+    }
+public:
+    int smallestDivisor(std::vector<int>& nums, int threshold) {
+        int left = 1;
+        int right = *max_element(nums.begin(), nums.end());
+        while(left <= right){
+            int mid = left + (right - left) / 2;
+            if (check(nums, mid, threshold)){
+                right = mid - 1;
+            }else{
+                left = mid + 1;
+            }
+        }
+        return left;
+    }
+};
